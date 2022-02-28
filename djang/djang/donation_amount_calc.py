dd8b29e857b2d4967d08e552025c1bf890ba64a0
damon <61207844+damonot@users.noreply.github.com>
2022-02-28 00:42:19 -0500

    fix: dynamic donos return as list
    previously returned as string

--- djang/djang/donation_amount_calc.py
+++ djang/djang/donation_amount_calc.py
@@ -19,20 +19,17 @@ def ipxapi(ip):
 
 def zip_to_income(data):
     ip_info = data
-    ip_info = json.loads(data) #pd.json_normalize(data)
-    print(ip_info)
-    ip_info = ip_info['zip']#.astype('str')
-    print(ip_info)
-    #ip_to_zip = ip_info.iloc[0]
+    ip_info = json.loads(data)
+    zip = ip_info['zip']
     
     cwd = os.getcwd()
     zip_table = pd.read_csv(cwd + '/djang/static/djang/US_Zip.csv',low_memory=False)
     us_median_income_table = zip_table.loc[:, ['NAME', 'S1901_C01_012E']]
     us_median_income_table['NAME'] = us_median_income_table['NAME'].map(lambda x: x.lstrip('ZCTA5').rstrip('aAbBcC'))
-    series = us_median_income_table['S1901_C01_012E'].where(us_median_income_table['NAME'] == ((' ')+ ip_info))
+    series = us_median_income_table['S1901_C01_012E'].where(us_median_income_table['NAME'] == ((' ')+ zip))
     series = series.dropna()
     result = int(series.iloc[0])
-    return json.dumps(result)
+    return (result)
 
 
 def income_to_donation(income,last_donation):
@@ -40,37 +37,38 @@ def income_to_donation(income,last_donation):
     median_income = int(income)
     
     if median_income < 45000:
-        return json.dumps(donation_amounts)
+        return (donation_amounts)
 
     elif 45000 <= median_income < 100000:
         if last_donation > 0:
             donation_amounts = [last_donation, last_donation*2,last_donation *3,last_donation *4,last_donation *5]
-            return json.dumps(donation_amounts)
+            return (donation_amounts)
         else: 
             donation_amounts  = [element * 2 for element in donation_amounts]
-            return json.dumps(donation_amounts)  
+            return (donation_amounts)  
 
     elif 100000 <= median_income < 200000:
         if last_donation > 0:
             donation_amounts = [last_donation, last_donation*2,last_donation *3,last_donation *4,last_donation *5]
-            return json.dumps(donation_amounts)
+            return (donation_amounts)
         else: 
             donation_amounts  = [element * 3 for element in donation_amounts]
-            return json.dumps(donation_amounts)
+            return (donation_amounts)
 
     elif median_income >= 200000:
         if last_donation > 0:
             donation_amounts = [last_donation, last_donation*2,last_donation *3,last_donation *4,last_donation *5]
-            return json.dumps(donation_amounts)
+            return (donation_amounts)
         else: 
             donation_amounts  = [element * 4 for element in donation_amounts]
-            return json.dumps(donation_amounts)
+            return (donation_amounts)
         
 
 def main(ip):
     apiresponse = ipxapi(ip)
     income = zip_to_income(apiresponse.text)
     donos = income_to_donation(income, 50)
-    print(donos)
+    print(type(donos))
+    return donos
 
 
