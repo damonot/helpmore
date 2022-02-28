81f1b794fd59c30d29a7ccb4498c805f29748bf6
damon <61207844+damonot@users.noreply.github.com>
2022-02-28 00:26:36 -0500

    feat: dynamic donos calculated

--- djang/djang/donation_amount_calc.py
+++ djang/djang/donation_amount_calc.py
@@ -1,56 +1,55 @@
 import pandas as pd
 import json
 import requests
-def zip_to_income(data):
-    ip_info = pd.json_normalize(data)
-    ip_info = ip_info['zip'].astype('str')
-    ip_to_zip = ip_info.iloc[0]
+import os
+
+def ipxapi(ip):
+    url = "https://ipxapi.com/api/ip?ip="+ip
     
-    zip_table = pd.read_csv('US_Zip.csv',low_memory=False)
+    headers = {
+        'Accept': "application/json",
+        'Content-Type': "application/json",
+        'Authorization': "Bearer 825|TYdYvsByrjJPNR7qk7srT1I1BJk5EEsPskHS4tk4",
+        'cache-control': "no-cache"
+    }
+    
+    response = requests.request("GET", url, headers=headers)
+    return response
+
+
+def zip_to_income(data):
+    ip_info = data
+    ip_info = json.loads(data) #pd.json_normalize(data)
+    print(ip_info)
+    ip_info = ip_info['zip']#.astype('str')
+    print(ip_info)
+    #ip_to_zip = ip_info.iloc[0]
+    
+    cwd = os.getcwd()
+    zip_table = pd.read_csv(cwd + '/djang/static/djang/US_Zip.csv',low_memory=False)
     us_median_income_table = zip_table.loc[:, ['NAME', 'S1901_C01_012E']]
     us_median_income_table['NAME'] = us_median_income_table['NAME'].map(lambda x: x.lstrip('ZCTA5').rstrip('aAbBcC'))
-    series = us_median_income_table['S1901_C01_012E'].where(us_median_income_table['NAME'] == ((' ')+ ip_to_zip))
+    series = us_median_income_table['S1901_C01_012E'].where(us_median_income_table['NAME'] == ((' ')+ ip_info))
     series = series.dropna()
     result = int(series.iloc[0])
     return json.dumps(result)
 
-#testing
-ip_address = '2601:741:8002:ded0:cb1:b9e5:9091:9144'
 
-url = "https://ipxapi.com/api/ip?ip="+ip_address
-                
-headers = {
-        'Accept': "application/json",
-        'Content-Type': "application/json",
-        'Authorization': "Bearer 765|lezHXaW8lTxWHPTgCjAeKHgv5rAZqlKhVax0AIut",
-        'cache-control': "no-cache"
-                    }
-response = requests.request("GET", url, headers=headers)
-data = response.json()
-median_income = zip_to_income(data)
-print(median_income)
-
-import json
 def income_to_donation(income,last_donation):
     donation_amounts = [25,50,100, 250, 500]
     median_income = int(income)
     
     if median_income < 45000:
         return json.dumps(donation_amounts)
-    
-    
-    
+
     elif 45000 <= median_income < 100000:
         if last_donation > 0:
             donation_amounts = [last_donation, last_donation*2,last_donation *3,last_donation *4,last_donation *5]
             return json.dumps(donation_amounts)
         else: 
             donation_amounts  = [element * 2 for element in donation_amounts]
-            return json.dumps(donation_amounts)
-        
-        
-        
-    
+            return json.dumps(donation_amounts)  
+
     elif 100000 <= median_income < 200000:
         if last_donation > 0:
             donation_amounts = [last_donation, last_donation*2,last_donation *3,last_donation *4,last_donation *5]
@@ -58,10 +57,7 @@ def income_to_donation(income,last_donation):
         else: 
             donation_amounts  = [element * 3 for element in donation_amounts]
             return json.dumps(donation_amounts)
-        
-        
-        
-    
+
     elif median_income >= 200000:
         if last_donation > 0:
             donation_amounts = [last_donation, last_donation*2,last_donation *3,last_donation *4,last_donation *5]
@@ -70,11 +66,11 @@ def income_to_donation(income,last_donation):
             donation_amounts  = [element * 4 for element in donation_amounts]
             return json.dumps(donation_amounts)
         
-        
-    
-        
-#Testing
 
-print(income_to_donation(median_income,0))
+def main(ip):
+    apiresponse = ipxapi(ip)
+    income = zip_to_income(apiresponse.text)
+    donos = income_to_donation(income, 50)
+    print(donos)
 
 
--- djang/djang/views.py
+++ djang/djang/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from requests import get
+from . import donation_amount_calc
 
 def index(request):
+
+    ip = get('https://api.ipify.org').text
+    recommented_dono = donation_amount_calc.main(ip)
+    #print(medianinc)
     context = {}
     return render(request, 'djang/index.html', context)
+    
