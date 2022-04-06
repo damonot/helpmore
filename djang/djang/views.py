52d557fefbcf55dcb1853b52d62a0daa0e840b45
damon <61207844+damonot@users.noreply.github.com>
2022-04-06 03:28:08 -0400

    fix: sync submitted and selected on form

--- djang/djang/views.py
+++ djang/djang/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from requests import get
 from . import donation_amount_calc, cookies, fiftyone_api
 
-from .forms import DonationForm, MyCustomForm
+from .forms import MyCustomForm
 
 
 def writedono(list):
@@ -20,7 +20,7 @@ def index(request):
 
     userinfo = fiftyone_api.main(request)
 
-    print(userinfo)
+    #print(userinfo)
 
     lastdono = cookies.getcookie(request)
     ip = get('https://api.ipify.org').text
@@ -28,33 +28,36 @@ def index(request):
     print(recdono)
 
     list_of_tuples=[    
-    ('0', str(recdono[0])),
-    ('1', str(recdono[1])),
-    ('2', str(recdono[2])),
-    ('3', str(recdono[3])),
-    ('4', str(recdono[4])),
+    (str(recdono[0]), str(recdono[0])),
+    (str(recdono[1]), str(recdono[1])),
+    (str(recdono[2]), str(recdono[2])),
+    (str(recdono[3]), str(recdono[3])),
+    (str(recdono[4]), str(recdono[4])),
     ]
-    
     form = MyCustomForm(request.POST, my_choices=list_of_tuples)
 
     context = {"recdono": recdono, "form": form}
     response = render(request, 'djang/index.html', context)
 
-    submitteddono = 0
-    print("SUMBITTED DONO " + str(submitteddono))
+    form_select = None
+    if form.is_valid():
+        form_select= int(form.cleaned_data.get("my_field"))
+
+    if form_select is None:
+        submitteddono = lastdono
+    else:
+        submitteddono = form_select
+    print("SELECTED: "+str(form_select)+ "\tSUBMITTED: "+str(submitteddono))
+
+    # print("SUMBITTED DONO " + str(submitteddono))
     cookies.setcookie(response, submitteddono)
 
     tip = int(submitteddono * .15)
     link = "https://link.justgiving.com/v1/charity/donate/charityId/13441?donationValue="+str(submitteddono)+"&totalAmount="+str(submitteddono+tip)+"&currency=GBP&skipGiftAid=true&skipMessage=true"
 
-    form_select = None
-    if form.is_valid():
-        form_select= form.cleaned_data.get("my_field")
-    
-    print("\n\n\nFORM DATA")
-    print(form_select)
+
 
     # print(link)
-    print("post request")
-    print(request.POST)
+    # print("post request")
+    # print(request.POST)
     return response
