03849eae29621e64d6204aa0499cf3416d2fdb2d
damon <61207844+damonot@users.noreply.github.com>
2022-04-20 16:07:43 -0400

    fix: min value on input

--- djang/djang/forms.py
+++ djang/djang/forms.py
@@ -5,7 +5,7 @@ class RadioForm(forms.Form):
     def __init__(self, *args, **kwargs):
         choices = kwargs.pop('my_choices')
         super(RadioForm, self).__init__(*args, **kwargs)
-        self.fields["my_field"] = forms.ChoiceField(choices=choices, widget=forms.RadioSelect, label='')
+        self.fields["my_radio"] = forms.ChoiceField(choices=choices, widget=forms.RadioSelect, label='')
 
 class DropdownForm(forms.Form):
     def __init__(self, *args, **kwargs):
@@ -14,6 +14,8 @@ class DropdownForm(forms.Form):
         self.fields["my_tip"] = forms.ChoiceField(choices=choices, widget=forms.Select, label='')
 
 class InputForm(forms.Form):
-    my_input = forms.IntegerField(required=False,label='')
+    my_input = forms.FloatField(required=False,label='', min_value=0,
+                            error_messages={'min_value': u'Price cannot be less than 0.00'})
+    
 
 
