624bd8c1abafd89674979dfd91ddf0a8c5997887
damon <61207844+damonot@users.noreply.github.com>
2022-04-20 16:09:35 -0400

    feat: proper linking to payment site

--- djang/djang/views.py
+++ djang/djang/views.py
@@ -4,13 +4,11 @@ from http import client
 from re import T, sub
 from django.shortcuts import render, redirect
 from requests import get
-from . import donation_amount_calc, cookies, fiftyone_api
+from . import donation_amount_calc, fiftyone_api
 from .forms import DropdownForm, RadioForm, InputForm
 
 
 def index(request):
-
-    cookies.main(request)
 
     userinfo = fiftyone_api.main(request) # Get MetaData
 
@@ -22,16 +20,13 @@ def index(request):
     except:
         client_ip = '8.8.8.8'
 
+    print("SESSION:" + str(request.session['previousDonation']))
     if 'previousDonation' in request.session:
-        request.session['previousDonation']
+        previousDonation = request.session['previousDonation']
     else:
-        request.session['previousDonation'] = 0
+        previousDonation = 0
 
-    request.session.modified = True
-
-    print('session '+str(request.session['previousDonation']))
-
-    previousDonation = request.session['previousDonation']
+    previousDonation = 0
 
     recommendedDonationsArray = donation_amount_calc.main(str(client_ip), previousDonation, userinfo)
     recomendedDonations = arrayToTupleList(recommendedDonationsArray)
@@ -45,43 +40,42 @@ def index(request):
     
     response = render(request, 'djang/index.html', context)
 
-
-    print("\n^CLIENT IP: "+str(client_ip)+\
-        "\nrecommendedDonations: "+(str(recommendedDonationsArray)))
-
     radio_select = 0
     input_select = '' 
-    print(radioDonations.is_valid())
-    print(inputDonation.is_valid())
+    tip_select = 0
+    print("RADIO valid? "+ str(radioDonations.is_valid()))
+
     if(radioDonations.is_valid()):
-        radio_select= int(radioDonations.cleaned_data.get("my_field"))
-        
+        radio_select= int(radioDonations.cleaned_data.get("my_radio"))
         tip_select = int(tipDropdowns.cleaned_data.get("my_tip")[1:-3])
         input_select = inputDonation['my_input'].value()
-        print("radio:" + str(radio_select))
-        print("tip:" + str(tip_select))
-        print("input:" + str(input_select))
 
         larger = computeLarger(radio_select, input_select)
 
+        print(str(radio_select) + " RADIO: " + str(radio_select))
+        print(str(larger) + " TIP: " + str(tip_select))
         total = int(larger) + int(tip_select)
+
+        request.session['previousDonation'] = total
+        request.session.modified = True
 
         link = "https://link.justgiving.com/v1/charity/donate/charityId/13441?donationValue="+str(larger)+"&totalAmount="+str(total)+"&currency=GBP&skipGiftAid=true&skipMessage=true"
         return redirect(link)
     else:
-        print("input type...")
-        print(type(input_select))
         input_select = inputDonation['my_input'].value()
-        if(type(input_select) == str):
-            if(input_select.isnumeric()):
-                tip_select = int(tipDropdowns.cleaned_data.get("my_tip")[1:-3])
-                print("input:" + str(input_select))       
-                print("tip:" + str(tip_select))
 
-                total = int(input_select) + int(tip_select)
+        if(type(input_select) == str and
+            len(input_select) != 0 and
+            input_select.isnumeric()):
+            print("TIP:"+ str(type(tip_select)) + " " + str(tip_select))
+            tip_select = int(tipDropdowns.cleaned_data.get("my_tip")[1:-3])
 
-                link = "https://link.justgiving.com/v1/charity/donate/charityId/13441?donationValue="+str(input_select)+"&totalAmount="+str(total)+"&currency=GBP&skipGiftAid=true&skipMessage=true"
-                return redirect(link)
+            total = int(input_select) + int(tip_select)
+            request.session['previousDonation'] = total
+            request.session.modified = True
+
+            link = "https://link.justgiving.com/v1/charity/donate/charityId/13441?donationValue="+str(input_select)+"&totalAmount="+str(total)+"&currency=GBP&skipGiftAid=true&skipMessage=true"
+            return redirect(link)
         else:  
             return response
 
@@ -105,8 +99,9 @@ def formatTip(arr):
 
 def computeLarger(radio_select, input_select):
 
-    if(type(input_select) == str):
-        if(input_select.isnumeric()):
+    if(type(input_select) == str and
+        len(input_select) != 0 and
+        input_select.isnumeric()):
             if(int(input_select) > int(radio_select)):
                 return input_select
             else:
